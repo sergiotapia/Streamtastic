919c6bf908b557c342fd719a0e4b2e5934479d73
Sergio Tapia Gutierrez <sergio@sergiotapia.com>
2012-06-02 23:29:45 -0400

    Added in Twitch.tv - huzzah!

--- Streamtastic/Streamtastic.Demo/Program.cs
+++ Streamtastic/Streamtastic.Demo/Program.cs
@@ -12,28 +12,41 @@ namespace Streamtastic.Demo
         {
             StreamFinder own3dStreamtastic = new StreamFinder(new Own3dStreamRepository());
 
-            // Let's find the most viewed streamers in general.
-            var topStreamers = own3dStreamtastic.FindTopViewedStreams();
-            foreach (var stream in topStreamers)
+            //// Let's find the most viewed streamers in general.
+            //var topStreamers = own3dStreamtastic.FindTopViewedStreams();
+            //foreach (var stream in topStreamers)
+            //{
+            //    Console.WriteLine(String.Format("{0} - Viewers: {1}", stream.Title, stream.ViewerCount));
+            //}
+
+            //// You can find the top viewed streamers for any game Own3d supports.
+            //// For example, League of Legends.
+            //var topStreamersForLeagueOfLegends = own3dStreamtastic.FindTopViewedStreamsByGame("League+of+Legends");
+            //foreach (var stream in topStreamersForLeagueOfLegends)
+            //{
+            //    Console.WriteLine(String.Format("{0} - Viewers: {1}", stream.Title, stream.ViewerCount));
+            //}
+
+            //// Or how about Dota 2.
+            //var topStreamersForDota2 = own3dStreamtastic.FindTopViewedStreamsByGame("Dota+2");
+            //foreach (var stream in topStreamersForDota2)
+            //{
+            //    Console.WriteLine(String.Format("{0} - Viewers: {1}", stream.Title, stream.ViewerCount));
+            //}
+
+            StreamFinder twitchTvStreamtastic = new StreamFinder(new TwitchtvStreamRepository());
+
+            var topStreamersTwitch = twitchTvStreamtastic.FindTopViewedStreams();
+            foreach (var stream in topStreamersTwitch)
             {
                 Console.WriteLine(String.Format("{0} - Viewers: {1}", stream.Title, stream.ViewerCount));
             }
 
-            // You can find the top viewed streamers for any game Own3d supports.
-            // For example, League of Legends.
-            var topStreamersForLeagueOfLegends = own3dStreamtastic.FindTopViewedStreamsByGame("League+of+Legends");
-            foreach (var stream in topStreamersForLeagueOfLegends)
+            var topStreamersTwitchForDota2 = twitchTvStreamtastic.FindTopViewedStreamsByGame("League of Legends");
+            foreach (var stream in topStreamersTwitchForDota2)
             {
                 Console.WriteLine(String.Format("{0} - Viewers: {1}", stream.Title, stream.ViewerCount));
             }
-
-            // Or how about Dota 2.
-            var topStreamersForDota2 = own3dStreamtastic.FindTopViewedStreamsByGame("Dota+2");
-            foreach (var stream in topStreamersForDota2)
-            {
-                Console.WriteLine(String.Format("{0} - Viewers: {1}", stream.Title, stream.ViewerCount));
-            }
-
             Console.ReadKey(true);
         }
     }
--- Streamtastic/Streamtastic/Concrete/TwitchtvStreamRepository.cs
+++ Streamtastic/Streamtastic/Concrete/TwitchtvStreamRepository.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Xml.Linq;
 using Streamtastic.Abstract;
 using Streamtastic.Models;
 
@@ -11,12 +13,43 @@ namespace Streamtastic.Concrete
     {
         public List<StreamModel> FindTopViewedStreams()
         {
-            throw new NotImplementedException();
+            string apiEndpoint = "http://api.justin.tv/api/stream/list.xml?category=gaming";
+
+            var topStreams = new List<StreamModel>();
+            var xmlDocument = XDocument.Load(apiEndpoint);
+
+            var streams = (from stream in xmlDocument.Descendants("stream")
+                           select new StreamModel()
+                                      {
+                                          Title = stream.Element("title").Value,
+                                          ViewerCount = Convert.ToInt32(stream.Element("channel_count").Value),
+                                          ThumbnailPreviewUrl = stream.Element("channel").Element("screen_cap_url_medium").Value,
+                                          ChannelUrl = stream.Element("channel").Element("channel_url").Value,
+                                          ChannelOwner = stream.Element("channel").Element("login").Value,
+                                      }).ToList<StreamModel>();
+
+            return streams;
         }
 
         public List<StreamModel> FindTopViewedStreamsByGame(string gameName)
         {
-            throw new NotImplementedException();
+            string apiEndpoint = "http://api.justin.tv/api/stream/list.xml?category=gaming";
+
+            var topStreams = new List<StreamModel>();
+            var xmlDocument = XDocument.Load(apiEndpoint);
+
+            var streams = (from stream in xmlDocument.Descendants("stream")
+                           where stream.Element("meta_game") != null && stream.Element("meta_game").Value == gameName
+                           select new StreamModel()
+                           {
+                               Title = stream.Element("title").Value,
+                               ViewerCount = Convert.ToInt32(stream.Element("channel_count").Value),
+                               ThumbnailPreviewUrl = stream.Element("channel").Element("screen_cap_url_medium").Value,
+                               ChannelUrl = stream.Element("channel").Element("channel_url").Value,
+                               ChannelOwner = stream.Element("channel").Element("login").Value,
+                           }).ToList<StreamModel>();
+
+            return streams;
         }
     }
 }
